27359704a9ac5dfefce578e47830ee26bf4b58b3
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-02-28 20:43:00 -0300

    version estable

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,12 +5,21 @@ function filter_modulos() {
     variable_pass = pass_input.value;
     if (variable_name === "gabriel" && variable_pass === "password") {        
         render_list();
+    } else if (variable_name === "maximiliano" && variable_pass === "maximiliano2023") {
+        render_list2();
     }
 }
 
 function render_list() {
     let lista = document.getElementById('lista');
     lista.innerHTML = `<li class="text-light fs-3"><a target="_blank" href="primero_segundo.html">1°Básico - 2°Básico. Matemática 📝</a></li>
+    <li class="text-light fs-3"><a target="_blank" href="tercero_medio_dispersion.html">3° Medio. Recuerdo Medidas de Tendencia Central: Promedio. Matemática 📝</a></li>
+    <li class="text-light fs-3"><a target="_blank" href="tercero_medio_dispersion_2.html">3° Medio. Medidas de Dispersión. Matemática 📝</a></li>`   
+}
+
+function render_list2() {
+    let lista = document.getElementById('lista');
+    lista.innerHTML = `
     <li class="text-light fs-3"><a target="_blank" href="tercero_medio_dispersion.html">3° Medio. Recuerdo Medidas de Tendencia Central: Promedio. Matemática 📝</a></li>
     <li class="text-light fs-3"><a target="_blank" href="tercero_medio_dispersion_2.html">3° Medio. Medidas de Dispersión. Matemática 📝</a></li>`   
 }
